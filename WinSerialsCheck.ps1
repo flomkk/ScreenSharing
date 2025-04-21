f09cf2474c9eeed413eaf01ba5b49604f2d7120a
flomkk <144499370+flomkk@users.noreply.github.com>
2025-04-21 18:24:18 +0200

    Update WinSerialsCheck.ps1

--- WinSerialsCheck.ps1
+++ WinSerialsCheck.ps1
@@ -18,19 +18,29 @@ $winuuid       = (Get-WmiObject Win32_ComputerSystemProduct).UUID
 $biosUUID      = (Get-WmiObject Win32_BIOS).SerialNumber
 $baseboardSN   = (Get-WmiObject Win32_BaseBoard).SerialNumber
 $cpuID         = (Get-WmiObject Win32_Processor | Select-Object -First 1).ProcessorId
-$diskSerial    = (Get-WmiObject Win32_PhysicalMedia | Select-Object -First 1).SerialNumber.Trim()
 $productID     = (Get-WmiObject Win32_OperatingSystem).SerialNumber
 $mac           = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.MACAddress -ne $null -and $_.IPEnabled } | Select-Object -First 1).MACAddress
 $sid           = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
 $machineGuid   = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\Cryptography' -Name 'MachineGuid'
 
 # ==== Output ====
-Write-Host -ForegroundColor Cyan " ================== SERIALS =================="
 Write-Host -ForegroundColor Cyan " Windows UUID           : $winuuid"
 Write-Host -ForegroundColor Cyan " BIOS UUID              : $biosUUID"
 Write-Host -ForegroundColor Cyan " Baseboard Serial       : $baseboardSN"
 Write-Host -ForegroundColor Cyan " Processor ID (CPU ID)  : $cpuID"
-Write-Host -ForegroundColor Cyan " Disk Serial Number     : $diskSerial"
+
+# ==== Disk Serial Numbers ====
+Write-Host -ForegroundColor Cyan " Disk Serial Numbers    :"
+$disks = Get-WmiObject Win32_PhysicalMedia
+$i = 0
+foreach ($disk in $disks) {
+    if ($disk.SerialNumber) {
+        $serial = $disk.SerialNumber.Trim()
+        Write-Host -ForegroundColor DarkCyan ("  Disk $i:`t$serial")
+        $i++
+    }
+}
+
 Write-Host -ForegroundColor Cyan " Windows Product ID     : $productID"
 Write-Host -ForegroundColor Cyan " MAC Address            : $mac"
 Write-Host -ForegroundColor Cyan " System SID             : $sid"
